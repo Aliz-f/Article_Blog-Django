1d2fc0fdbebc9ac0cd5963bacffad7a4465cb94a
Aliz-f <fad127alireza2@gmail.com>
2021-02-13 17:46:32 +0330

    add templates and static files for our pages

--- article/blog/views.py
+++ article/blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+
+    {
+        'author': 'Alirzea',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'Feb 13, 2021'
+    },
+    {
+        'author': 'Mohammad',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'Feb 14, 2021'
+    }
+
+]
+
 def home (request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def about (request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
+    
